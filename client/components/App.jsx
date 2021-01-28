f824a32aaa24bda421d7b0cf4cbc6c6d2b1c52ae
cphilathong <cphilathong1@gmail.com>
2021-01-29 11:51:46 +1300

    merge stash

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 
 import Home from './Home'
+import Ingredients from './Ingredients'
 
 const App = () => {
   return (
     <>
       <Home />
+      <Route exact path="/:flavour" component={Ingredients} />
     </>
   )
 }
