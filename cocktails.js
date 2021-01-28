590e5137dfd8f157deaffc3b3906ff188df9e831
sirchras <cjlynch@hotmail.co.nz>
2021-01-28 18:41:49 +1300

    trim cocktail dataset

---
+++ cocktails.js
@@ -0,0 +1,76 @@
+const cocktails = [
+  {
+    image: 'http://cocktail.itsplanet.eu/img/cocktail/2017_15.jpg',
+    name: 'Daiquiri',
+    flavor: 'Fruity',
+    preparation: 'Rum (White) (45) Sweet & Sour (30)'
+  },
+  {
+    image: 'http://cocktail.itsplanet.eu/img/cocktail/2017_50.jpg',
+    name: 'Old Fashioned',
+    flavor: 'Sweet',
+    preparation: 'Whiskey (45) Bitter Campari (5) Soda (20)'
+  },
+  {
+    image: 'http://cocktail.itsplanet.eu/img/cocktail/2017_45.jpg',
+    name: 'Mojito',
+    flavor: 'Sweet',
+    preparation: 'Rum (White) (60) Lime (30) Soda (50)'
+  },
+  {
+    image: 'http://cocktail.itsplanet.eu/img/cocktail/2017_40.jpg',
+    name: 'Margarita',
+    flavor: 'Sweet',
+    preparation: 'Lime (25) Triple Sec (25) Tequila (50)'
+  },
+  {
+    image: 'http://cocktail.itsplanet.eu/img/cocktail/2017_413.jpg',
+    name: 'Whiskey Sour',
+    flavor: 'Sour',
+    preparation: 'Whiskey (45) Sweet & Sour (75)'
+  },
+  {
+    image: 'http://cocktail.itsplanet.eu/img/cocktail/2017_53.jpg',
+    name: 'Pina Colada',
+    flavor: 'Fruity',
+    preparation: 'Rum (White) (45) Pineapple Juice (100) Coconut Syrup (20)'
+  },
+  {
+    image: 'http://cocktail.itsplanet.eu/img/cocktail/2017_13.jpg',
+    name: 'Cosmopolitan',
+    flavor: 'Fruity',
+    preparation: 'Vodka (40) Triple Sec (15) Lime (30) Cranberry Juice (60)'
+  },
+  {
+    image: '',
+    name: 'Pisco Sour',
+    flavor: 'Sour',
+    preparation: ''
+  },
+  {
+    image: '',
+    name: 'Amaretto Sour',
+    flavor: 'Sour',
+    preparation: ''
+  },
+  {
+    image: '',
+    name: 'Bloody Mary',
+    flavor: 'Spicy',
+    preparation: ''
+  },
+  {
+    image: '',
+    name: 'Penicillin',
+    flavor: 'Spicy',
+    preparation: ''
+  },
+  {
+    image: '',
+    name: 'Bon Fire',
+    flavor: 'Spicy',
+    preparation: ''
+  }
+]
+
+export default cocktails
