4ceb1168ce7b5213a130bf363decdf6c1c027305
Anna McCormack <anna.mccormack94@gmail.com>
2021-01-29 10:46:57 +1300

    update data

---
+++ data/cocktails.js
@@ -0,0 +1,100 @@
+const cocktails = [
+  {
+    id: 1,
+    image: '/images/strawberry-daiq.jpg',
+    name: 'Strawberry Daiquiri',
+    flavor: 'Fruity',
+    ingredients: ['Strawberries', 'White Rum', 'Lime', 'Sugar Syrup'],
+    preparation: ['Combine ice, sugar, and strawberries. Add in lime juice, and rum. Blend until smooth. Pour into glasses and serve.']
+  },
+  {
+    id: 2,
+    image: '/images/old-fashioned.jpg',
+    name: 'Old Fashioned',
+    flavor: 'Sweet',
+    ingredients: ['Orange', 'Bourbon', 'Bitters', 'Sugar'],
+    preparation: ['Dissolve or muddle sugar with a splash of water in an Old Fashioned glass. Fill glass ¾ full of ice. Add Bourbon and bitters and stir. Garnish with orange slice and cherry.']
+  },
+  {
+    id: 3,
+    image: '/images/mojito.jpg',
+    name: 'Mojito',
+    flavor: 'Sweet',
+    ingredients: ['Mint', 'White Rum', 'Soda Water', 'Lime', 'Simple Syrup'],
+    preparation: ['In a tall highball glass muddle mint with lime juice and simple syrup. Add crushed ice and rum then stir. Top up with crushed ice, add soda, and stir one more time. Garnish with a sprig of mint.']
+  },
+  {
+    id: 4,
+    image: '/images/rasp-marg.jpg',
+    name: 'Raspberry Margarita',
+    flavor: 'Sweet',
+    ingredients: ['Raspberries', 'Tequila', 'Triple Sec', 'Lime Juice'],
+    preparation: ['Pour the tequila, triple sec, lime juice, fresh raspberries, and ice cubes into a blender. Blend until smooth. Pour the contents into a chilled margarita glass. Garnish with fresh raspberries and serve.']
+  },
+  {
+    id: 5,
+    image: '/images/whiskey-sour.jpg',
+    name: 'Whiskey Sour',
+    flavor: 'Sour',
+    ingredients: [],
+    preparation: ['Whiskey (45) Sweet & Sour (75)']
+  },
+  {
+    id: 6,
+    image: '/images/pina-colada.jpeg',
+    name: 'Pina Colada',
+    flavor: 'Fruity',
+    ingredients: ['Pineapple Juice', 'White Rum', 'Coconut Cream'],
+    preparation: ['Mix with crushed ice until smooth. Pour into chilled glass, garnish with a pineapple wedge, and serve.']
+  },
+  {
+    id: 7,
+    image: '/images/cosmo.jpg',
+    name: 'Cosmopolitan',
+    flavor: 'Fruity',
+    ingredients: ['Cranberry Juice', 'Lime', 'Vodka', 'Triple Sec'],
+    preparation: ['Combine all ingredients into an ice-filled shaker and shake until well chilled. Strain into a chilled martini glass. Garnish with a lime wheel and cranberries.']
+  },
+  {
+    id: 8,
+    image: '/images/pisco-sour.jpg',
+    name: 'Pisco Sour',
+    flavor: 'Sour',
+    ingredients: ['Lemon Juice', 'Egg White', 'Pisco', 'Simple Syrup'],
+    preparation: ['Vigorously shake and strain contents in a cocktail shaker with ice cubes, then pour into glass and garnish with bitters.']
+  },
+  {
+    id: 9,
+    image: '/images/amaretto-sour.jpg',
+    name: 'Amaretto Sour',
+    flavor: 'Sour',
+    ingredients: ['Lemon Juice', 'Lemon-Lime Soda', 'Amaretto Liqueur', 'Bourbon', 'Sugar'],
+    preparation: ['Shake with ice in a cocktail shaker and strain into a glass. Top with lemon lime soda. Garnish with a lemon slice and cherry.']
+  },
+  {
+    id: 10,
+    image: '/images/bloody-mary.jpg',
+    name: 'Bloody Mary',
+    flavor: 'Spicy',
+    ingredients: ['Tomato Juice', 'Vodka', 'Tabasco Sauce', 'Worchestershire Sauce'],
+    preparation: ['Add dashes of Worcestershire Sauce, Tabasco, salt and pepper into highball glass, then pour all ingredients into highball with ice cubes. Stir gently. Garnish with celery stalk.']
+  },
+  {
+    id: 11,
+    image: '/images/penicillin-cocktail.jpg',
+    name: 'Penicillin',
+    flavor: 'Spicy',
+    ingredients: ['Ginger', 'Scotch', 'Lemon', 'Honey'],
+    preparation: ['Combine all ingredients in a shaker with ice, and shake until well-chilled. Strain into a rocks glass over fresh ice. Top with the Islay single malt scotch. Garnish with a piece of candied ginger.']
+  },
+  {
+    id: 12,
+    image: '/images/michelada.jpg',
+    name: 'Michelada',
+    flavor: 'Spicy',
+    ingredients: ['Beer', 'Tomato Juice', 'Lime', 'Tabasco Sauce', 'Worchestershire Sauce'],
+    preparation: ['Mix the beer with tomato juice, freshly squeezed lime juice, Worcestershire sauce, and hot sauce.']
+  }
+]
+
+export default cocktails
