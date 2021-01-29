ddac06e78ab563a6ce5a913543c1675753b35b9e
cphilathong <cphilathong1@gmail.com>
2021-01-29 14:03:21 +1300

    update ingredients component

--- client/components/Ingredients.jsx
+++ client/components/Ingredients.jsx
@@ -3,14 +3,31 @@ import hash from 'hash-string'
 
 import ImageCard from './ImageCard'
 
-import ingredients from '../../data/ingredients'
+import ingr from '../../data/ingredients'
+import cocktails from '../../data/cocktails'
 
 function Ingredients (props) {
+  const drinks = cocktails.filter((cocktail) => {
+    return cocktail.flavor.toLowerCase() === props.match.params.flavour
+  })
+
+  const mainIngr = drinks.map(drink => {
+    console.log(typeof drink)
+    return {
+      name: drink.ingredients[0],
+      image: ingr.find(itm => itm.name === drink.ingredients[0]).image,
+      drinkId: drink.id
+    }
+  })
+
+  console.log('matching ingredients:', mainIngr)
+  console.log('matching drinks:', drinks)
+  console.log('ingredients:', ingr)
   return (
     <div className='container'>
       <h1>Choose an Ingredient</h1>
       {
-        ingredients.map(ingredient => (
+        mainIngr.map(ingredient => (
           <ImageCard key={hash(ingredient.name)} item={ingredient} />
         ))
       }
--- data/ingredients.js
+++ data/ingredients.js
@@ -1,52 +1,52 @@
 const ingredients = [
-    {
-        name: 'Strawberries' ,
-        image:'/images/0-strawbs.jpg'
-    },
-    {
-        name: 'Orange',
-        image: '/images/0-oranges.jpg'
-    },
-    {
-        name: 'Mint',
-        image: '/images/0-mint.jpg'
-    },
-    {
-        name: 'Raspberries',
-        image: '/images/0-rasps.jpg'
-    },
-    {
-        name: 'Lemon Juice',
-        image: '/images/0-lemons.jpg'
-    },
-    {
-        name: 'Pineapple Juice',
-        image: '/images/0-pineapples.jpg'
-    },
-    {
-        name: 'Cranberry Juice',
-        image: '/images/0-cranberries.jpg'
-    },
-    {
-        name: 'Tomato Juice',
-        image: '/images/0-tomatoes.jpg'
-    },
-    {
-        name: 'Ginger',
-        image: '/images/0-ginger.jpg'
-    },
-    {
-        name: 'Tabasco Sauce',
-        image: '/images/0-tabasco.jpg'
-    },
-    {
-        name: 'Lemon & Lime',
-        image: '/images/0-lemon-lime.jpg'
-    },
-    {
-        name: 'Cherry',
-        image: '/images/0-cherries.jpg'
-    }
+  {
+    name: 'Strawberries',
+    image: '/images/0-strawbs.jpg'
+  },
+  {
+    name: 'Orange',
+    image: '/images/0-oranges.jpg'
+  },
+  {
+    name: 'Mint',
+    image: '/images/0-mint.jpg'
+  },
+  {
+    name: 'Raspberries',
+    image: '/images/0-rasps.jpg'
+  },
+  {
+    name: 'Lemon Juice',
+    image: '/images/0-lemons.jpg'
+  },
+  {
+    name: 'Pineapple Juice',
+    image: '/images/0-pineapples.jpg'
+  },
+  {
+    name: 'Cranberry Juice',
+    image: '/images/0-cranberries.jpg'
+  },
+  {
+    name: 'Tomato Juice',
+    image: '/images/0-tomatoes.jpg'
+  },
+  {
+    name: 'Ginger',
+    image: '/images/0-ginger.jpg'
+  },
+  {
+    name: 'Tabasco Sauce',
+    image: '/images/0-tabasco.jpg'
+  },
+  {
+    name: 'Lemon & Lime',
+    image: '/images/0-lemon-lime.jpg'
+  },
+  {
+    name: 'Cherry',
+    image: '/images/0-cherries.jpg'
+  }
 ]
 
 export default ingredients
